366812796dea7f5f461e78eaf7fa7da199af3694
mitrajesse <mitrajesse@gmail.com>
2024-10-03 17:22:33 -0400

    Initial Commit

---
+++ Lab06/Lab06App.swift
@@ -0,0 +1,17 @@
+//
+//  Lab06App.swift
+//  Lab06
+//
+//  Created by Jesse Mitra on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab06App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
